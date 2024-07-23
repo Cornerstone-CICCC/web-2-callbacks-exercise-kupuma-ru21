0cce9504c35bc022708c0c1bfee5b629b7ab2978
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-07-23 15:50:00 -0700

    chore: change codes

--- file_reader.js
+++ file_reader.js
@@ -1,47 +1,18 @@
 const fs = require("fs");
 
-const getFirstName = (getLastName, getAge, getHobbies) => {
-  fs.readFile("./firstname.txt", "utf-8", (err, firstName) => {
-    if (err) {
-      console.log("Error: ", err);
-      return;
-    }
-    getLastName(firstName, getAge, getHobbies);
+fs.readFile("./firstname.txt", "utf-8", (err, firstname) => {
+  if (err) throw err;
+  fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+    if (err) throw err;
+    fs.readFile("./age.txt", "utf-8", (err, age) => {
+      if (err) throw err;
+      fs.readFile("./hobbies.txt", "utf-8", (err, hobbies) => {
+        if (err) throw err;
+        const hobbiesArray = JSON.parse(hobbies);
+        console.log(
+          `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbiesArray[0]} and ${hobbiesArray[1]}`
+        );
+      });
+    });
   });
-};
-
-const getLastName = (firstName, getAge, getHobbies) => {
-  fs.readFile("./lastname.txt", "utf-8", (err, lastName) => {
-    if (err) {
-      console.log("Error: ", err);
-      return;
-    }
-    getAge(`${firstName + " " + lastName} is`, getHobbies);
-  });
-};
-
-const getAge = (fullName, getHobbies) => {
-  fs.readFile("./age.txt", "utf-8", (err, age) => {
-    if (err) {
-      console.log("Error: ", err);
-      return;
-    }
-    getHobbies(`${fullName} ${age} years old`);
-  });
-};
-
-const getHobbies = (fullNameAndAge) => {
-  fs.readFile("./hobbies.txt", "utf-8", (err, hobbies) => {
-    if (err) {
-      console.log("Error: ", err);
-      return;
-    }
-    const hobbiesArray = JSON.parse(hobbies);
-    console.log(
-      "result: ",
-      `${fullNameAndAge} and his hobbies are ${hobbiesArray.join(" and ")}`
-    );
-  });
-};
-
-getFirstName(getLastName, getAge, getHobbies);
+});
